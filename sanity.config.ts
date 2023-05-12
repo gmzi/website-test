b1aa8fa29b9a75b81ec6807e042ab9bc1ce445a3
gmzi <gmzieres@gmail.com>
2023-05-12 19:09:53 -0400

    change studio id

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import {deskTool} from "sanity/desk";
 import schemas from './sanity/schemas/index';
 
 const config = defineConfig({
-    projectId: "ms3bty93",
+    projectId: "sl89nds0",
     dataset: "production",
     title: "Test website",
     apiVersion: "2023-05-11",
--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,5 +1,7 @@
 const config = {
-    projectId: "ms3bty93",
+    // old one:
+    // projectId: "ms3bty93",
+    projectId: "sl89nds0",
     dataset: "production",
     apiVersion: "2023-05-11",
     useCdn: false
